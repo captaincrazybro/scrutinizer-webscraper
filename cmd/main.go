2bbffdccaa795b830d589e5d259ed50e56b46138
captaincrazybro <myeyesareonmizar@gmail.com>
2021-06-22 09:44:54 -0400

    Initialized project

---
+++ cmd/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Scrutinizer Webscraper started!")
+	// TODO add support for lambda
+}
---
+++ login.go
@@ -0,0 +1,23 @@
+package sw
+
+import (
+	lu "github.com/captaincrazybro/literalutil"
+)
+
+// Login logs into scrutinizer, retrieves the session cookies and returns them
+func Login() lu.String {
+
+	return ""
+}
+
+// getCSRPToken retrieves the CSRP token from the login page button
+func getCSRPToken() lu.String {
+
+	return ""
+}
+
+// loginCheck POSTs to the /login_check URL and retrieves the cookies, returning them
+func loginCheck() lu.String {
+
+	return ""
+}
