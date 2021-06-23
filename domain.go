7d30bcb08b13dc6e7532eeffd543d81e5f490812
cgordon28 <camerongordon111@gmail.com>
2021-06-23 16:08:06 -0400

    Got main.go to retrieve repos and score average

--- domain.go
+++ domain.go
@@ -1,9 +1,14 @@
 package sw
 
 const (
-	Endpoint     string = "https://scrutinizer-ci.com/"
-	LoginPageURL string = Endpoint + "login#"
-	ReposPageURL string = Endpoint + "dashboard/repositories"
-	BBOrgName    string = "silintl"
-	GHOrgName    string = "silinternational"
+	Endpoint     	string = "https://scrutinizer-ci.com/"
+	LoginPageURL 	string = Endpoint + "login#"
+	ReposPageURL	string = Endpoint + "dashboard/repositories"
+	BBOrgName    	string = "silintl"
+	GHOrgName    	string = "silinternational"
+	DivStyle		string = `text-align: center; color:#fff; font-size:18px; font-weight: bold;  background-repeat: repeat-x; background-image:linear-gradient(to bottom,
+		#27ae60, #2c9244, #2c9244, #27ae60
+);
+background-color: #CCC; background-repeat: repeat-x; border:rgba(0, 0, 0, 0.1) rgba(0, 0, 0, 0.1) rgba(0, 0, 0, 0.25);
+padding:11px 15px; `
 )
