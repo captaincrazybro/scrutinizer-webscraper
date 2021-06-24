39b4ebcb862b7be942ea6d5e3594ca725f268a61
captaincrazybro <myeyesareonmizar@gmail.com>
2021-06-24 13:43:44 -0400

    updated quality-score lambda

--- quality-score/main.go
+++ quality-score/main.go
@@ -1,5 +1,15 @@
 package main
 
 func main() {
+	// starts the lambda
+	//fmt.Println("Lambda started!")
+	//lambda.Start(handleSchedule)
+
+	// testing purposes
+	handleSchedule()
+}
+
+// handleSchedule function to call once every week
+func handleSchedule() {
 
 }
